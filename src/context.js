3836fee9e858be037d38d2091e4e3e2e4d27dd33
Pazhiloi <martinsen06@gmail.com>
2022-02-11 12:52:09 +0200

    first commit

---
+++ src/context.js
@@ -0,0 +1,16 @@
+import React, { useState, useContext, useEffect } from 'react'
+import useFetch from './useFetch'
+const AppContext = React.createContext()
+
+const AppProvider = ({ children }) => {
+  const [query, setQuery] = useState('dark')
+  const {isLoading, error, data: movies} = useFetch(`&s=${query}`)
+
+  
+  return <AppContext.Provider value={{isLoading, error, movies, query, setQuery}}>{children}</AppContext.Provider>
+}
+export const useGlobalContext = () => {
+  return useContext(AppContext)
+}
+
+export { AppContext, AppProvider }
